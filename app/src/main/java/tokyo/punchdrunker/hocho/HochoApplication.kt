f24f540a66ce19ef5ff854e834e501936c3efff7
punchdrunker <nanao.hhk@gmail.com>
2018-02-05 01:26:19 +0900

    use leakcanary

--- app/src/main/java/tokyo/punchdrunker/hocho/HochoApplication.kt
+++ app/src/main/java/tokyo/punchdrunker/hocho/HochoApplication.kt
@@ -2,14 +2,22 @@ package tokyo.punchdrunker.hocho
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
 
 class HochoApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
         // デバッグビルド限定でデバッグ用ツールの設定
         if (BuildConfig.DEBUG) {
+
             Timber.plant(Timber.DebugTree())
             Stetho.initializeWithDefaults(this)
         }
