913f5454ca983d77e5330562c1eebde17d201566
punchdrunker <nanao.hhk@gmail.com>
2018-10-05 14:27:44 +0900

    add progress bars

--- app/src/main/java/tokyo/punchdrunker/hocho/ProgressActivity.kt
+++ app/src/main/java/tokyo/punchdrunker/hocho/ProgressActivity.kt
@@ -1,8 +1,10 @@
 package tokyo.punchdrunker.hocho
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.activity_progress.*
 
 import tokyo.punchdrunker.hocho.databinding.ActivityProgressBinding
 
@@ -16,12 +18,27 @@ class ProgressActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setupToolbar()
 
-        binding.customButton.setOnClickListener {button -> (button as CustomButton).toggle()}
+        binding.activity = this
+        binding.customButton.setOnClickListener { button ->
+            (button as CustomButton).toggle()
+        }
+    }
+
+    fun setProgress0(view: View) {
+        progress3.progress = 0
+    }
+
+    fun setProgress50(view: View) {
+        progress3.progress = 50
+    }
+
+    fun setProgress100(view: View) {
+        progress3.progress = 100
     }
 
     private fun setupToolbar() {
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.apply{
+        supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             setHomeButtonEnabled(true)
             title = ""
