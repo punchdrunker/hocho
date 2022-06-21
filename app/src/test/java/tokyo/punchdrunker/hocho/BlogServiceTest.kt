ac1fbcfc356f1e0d78bccc3f64e3571d6776d535
punchdrunker <nanao.hhk@gmail.com>
2022-06-22 01:08:31 +0900

    add ktx for test

--- app/src/test/java/tokyo/punchdrunker/hocho/BlogServiceTest.kt
+++ app/src/test/java/tokyo/punchdrunker/hocho/BlogServiceTest.kt
@@ -19,32 +19,44 @@ class BlogServiceTest {
     @Test
     fun fetchFeed() {
         val stream = javaClass.classLoader!!.getResourceAsStream("android-developer-blog.xml")
-        val mockXmlString = stream.bufferedReader().use { it.readText()}
+        val mockXmlString = stream.bufferedReader().use { it.readText() }
         val response = createMockResponse(mockXmlString)
         server.enqueue(response)
         server.start()
 
         val service = createService()
         val actual = service.fetch("rss")
-                .execute()
+            .execute()
         actual.body()!!.entryList!!.run {
             assertEquals(25, size)
-            assertEquals("Creating an app to help your community during the pandemic with Gaston Saillen #IamaGDE", this[0].title)
+            assertEquals(
+                "Creating an app to help your community during the pandemic with Gaston Saillen #IamaGDE",
+                this[0].title
+            )
             assertEquals("2021/12/20 9:59", this[0].dateForDisplay())
-            assertEquals("http://android-developers.googleblog.com/2021/12/creating-app-to-help-your-community.html", this[0].articleUrl())
-            assertEquals("Posted by Alicja Heisig, Developer Relations Program Manager   Welcome to #IamaGDE - a series of spotlights presenting Google Developer Expe...", this[0].shortContent())
-            assertEquals("https://blogger.googleusercontent.com/img/a/AVvXsEgHl1OMxu_r7B6C2OHY0OsAyNn8j00v_A2ASjMKfjrw2JO0B9fbD6-5uohhucBeSLh_6ngRqhyARan_Tn1Sd-veSxOc3b-Lo1H_Q0PktyH5hrpCkhX54T8PoLWqFlfkRWIIy_0GWkK84s1CgfSk8w05GanB28I5mnb4vDPrHqG0CBmkcNuiqNygPjgP", this[0].imageUrl())
+            assertEquals(
+                "http://android-developers.googleblog.com/2021/12/creating-app-to-help-your-community.html",
+                this[0].articleUrl()
+            )
+            assertEquals(
+                "Posted by Alicja Heisig, Developer Relations Program Manager   Welcome to #IamaGDE - a series of spotlights presenting Google Developer Expe...",
+                this[0].shortContent()
+            )
+            assertEquals(
+                "https://blogger.googleusercontent.com/img/a/AVvXsEgHl1OMxu_r7B6C2OHY0OsAyNn8j00v_A2ASjMKfjrw2JO0B9fbD6-5uohhucBeSLh_6ngRqhyARan_Tn1Sd-veSxOc3b-Lo1H_Q0PktyH5hrpCkhX54T8PoLWqFlfkRWIIy_0GWkK84s1CgfSk8w05GanB28I5mnb4vDPrHqG0CBmkcNuiqNygPjgP",
+                this[0].imageUrl()
+            )
         }
     }
 
     private fun createMockResponse(mockBody: String) = MockResponse()
-            .addHeader("Content-Type", "application/xml")
-            .setResponseCode(200)
-            .setBody(mockBody)
+        .addHeader("Content-Type", "application/xml")
+        .setResponseCode(200)
+        .setBody(mockBody)
 
     private fun createService() = Retrofit.Builder()
-            .baseUrl(server.url("/").toString())
-            .addConverterFactory(SimpleXmlConverterFactory.create())
-            .build()
-            .create(BlogService::class.java)
+        .baseUrl(server.url("/").toString())
+        .addConverterFactory(SimpleXmlConverterFactory.create())
+        .build()
+        .create(BlogService::class.java)
 }
